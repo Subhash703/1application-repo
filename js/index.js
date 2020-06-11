5d8c194df8e0ad4b04674ce5dab9719c2ce3ee01
Subhash Chandra <49841848+Subhash703@users.noreply.github.com>
2020-06-11 15:00:34 +0530

    All files uploaded

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function animateNext() {
+    var elements = document.querySelectorAll('.one-c-left img:not(.show)');
+    if (elements.length) {
+      elements[0].classList.add('show');
+      setTimeout(animateNext,1000);
+    }
+    
+    const flyingRocket = document.querySelectorAll('.one-c-left img');
+    flyingRocket.forEach(item =>{
+      console.log(item);
+      item.classList.add("flying-rocket");
+    });
+
+  }
+  
+  animateNext();
+
+  
